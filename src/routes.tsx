8a265cafdfc7836504466458e5e5ad681c3a4091
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-19 09:33:54 +0800

    61-routingWithReactRouter-settingUpRouting

---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import GameDetailPage from "./pages/GameDetailPage";
+import HomPage from "./pages/HomPage";
+import Layout from "./pages/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomPage /> },
+      { path: "games/:id", element: <GameDetailPage /> },
+    ],
+  },
+]);
+
+export default router;
