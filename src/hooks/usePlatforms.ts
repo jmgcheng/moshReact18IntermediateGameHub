f8fd861a9f83e2836922ebb2622e988381e9c367
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-19 12:00:58 +0800

    64-routingWithReactRouter-exerciseRefactoringEntities

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,21 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
+import { Platform } from "../entities/Platform";
 
-const apiClient = new APIClient<Platform>('/platforms/lists/parents');
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
-
-const usePlatforms = () => 
+const usePlatforms = () =>
   useQuery({
-    queryKey: ['platforms'],
+    queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: ms('24h'),
-    initialData: platforms
-  })
+    staleTime: ms("24h"),
+    initialData: platforms,
+  });
 
 export default usePlatforms;
